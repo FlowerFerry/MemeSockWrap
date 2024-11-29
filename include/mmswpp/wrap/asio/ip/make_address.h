0d65d9c3d774e875c1cf78bf9ee485f9c5f3e915
FlowerFerry <flower_ferry@outlook.com>
2024-11-29 20:08:24 +0800

    -

---
+++ include/mmswpp/wrap/asio/ip/make_address.h
@@ -0,0 +1,61 @@
+
+#ifndef MMSWPP_WRAP_ASIO_IP_MAKE_ADDRESS_H_INCLUDED
+#define MMSWPP_WRAP_ASIO_IP_MAKE_ADDRESS_H_INCLUDED
+
+#include <asio/ip/address.hpp>
+#include <memepp/string.hpp>
+
+namespace mmswpp {
+namespace wrap {
+namespace asio {
+namespace ip {
+
+inline ::asio::ip::address_v6 make_address_v6(const memepp::string& _str, ::asio::error_code& _ec)
+{
+    auto addr4 = ::asio::ip::make_address_v4(_str.data(), _ec);
+    if (!_ec) {
+        return ::asio::ip::make_address_v6(::asio::ip::v4_mapped, addr4);
+    }
+    return ::asio::ip::make_address_v6(_str.data(), _ec);
+}
+
+inline ::asio::ip::address_v6 make_address_v6(const memepp::string& _str)
+{
+    ::asio::error_code ec;
+    auto addr = make_address_v6(_str, ec);
+    if (ec) {
+        throw ::asio::system_error(ec);
+    }
+    return addr;
+}
+
+inline ::asio::ip::address_v4 make_address_v4(const memepp::string& _str, ::asio::error_code& _ec)
+{  
+    auto addr6 = asio::ip::make_address_v6(str, ec);
+    if (!ec) {
+        if (addr6.is_v4_mapped()) {
+            return ::asio::ip::make_address_v4(::asio::ip::v4_mapped, addr6);
+        }
+        else
+            return {};
+    }
+
+    return ::asio::ip::make_address_v4(_str.data(), _ec);
+}
+
+inline ::asio::ip::address_v4 make_address_v4(const memepp::string& _str)
+{
+    ::asio::error_code ec;
+    auto addr = make_address_v4(_str, ec);
+    if (ec) {
+        throw ::asio::system_error(ec);
+    }
+    return addr;
+}
+
+} // namespace mmswpp::wrap::asio::ip
+} // namespace mmswpp::wrap::asio
+} // namespace mmswpp::wrap
+} // namespace mmswpp
+
+#endif // !MMSWPP_WRAP_ASIO_IP_MAKE_ADDRESS_H_INCLUDED
